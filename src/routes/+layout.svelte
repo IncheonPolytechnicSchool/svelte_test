d439b2d439c6bd313c61c8ca131fa2b4159a889b
choebyeong-gwan <ipschool22@gmail.com>
2023-01-06 14:17:24 +0900

    4commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<nav class="navbar">
+    <div class="navbar__logo">
+      <i class="fa fa-users"></i>
+      <a class="white" href="index.html">Study</a>
+    </div>
+
+    <ul class="navbar__menu">
+      <li>
+        <div class="dropdown">
+          <button class="dropbtn">Study
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="/code">Code</a>
+          <a href="pages/study_ing.html">ing</a>
+          <a href="pages/book_complete.html">Book_Complete</a>
+        </div>
+      </div>
+      </li>
+      <li>
+        <a class="white" 
+             href="pages/exercise.html">
+             Exercise
+        </a>
+      </li>
+      <li>
+        <div class="dropdown">
+          <button class="dropbtn">Film
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="pages/film_history.html">History</a>
+          <a href="pages/film_history_1945.html">1945</a>
+        </div>
+      </div>
+      </li>
+      <li>
+        <div class="dropdown">
+          <button class="dropbtn">0
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="pages/0_booking.html">booking</a>
+          <a href="pages/0_goods.html">booking</a>
+          <a href="pages/0_tiamo.htmm">tiamo</a>
+        </div>
+      </div>
+      </li>
+   </ul>
+
+    <ul class="navbar__icons">
+      <li><i class="fa fa-youtube"></i></li>
+      <li><i class="fa fa-instagram"></i></li>
+    </ul>
+</nav>
+<slot></slot>
