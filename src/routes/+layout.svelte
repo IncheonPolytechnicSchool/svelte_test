26ca0219a5540edeff5be88791277de62cedd753
choebyeong-gwan <ipschool22@gmail.com>
2023-01-06 14:46:53 +0900

    8commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,56 +1,12 @@
-<nav class="navbar">
-    <div class="navbar__logo">
-      <i class="fa fa-users"></i>
-      <a class="white" href="index.html">Study</a>
-    </div>
-
-    <ul class="navbar__menu">
-      <li>
-        <div class="dropdown">
-          <button class="dropbtn">Study
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="/code">code</a>
-          <a href="pages/study_ing.html">ing</a>
-          <a href="pages/book_complete.html">Book_Complete</a>
-        </div>
-      </div>
-      </li>
-      <li>
-        <a class="white" 
-             href="pages/exercise.html">
-             Exercise
-        </a>
-      </li>
-      <li>
-        <div class="dropdown">
-          <button class="dropbtn">Film
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="pages/film_history.html">History</a>
-          <a href="pages/film_history_1945.html">1945</a>
-        </div>
-      </div>
-      </li>
-      <li>
-        <div class="dropdown">
-          <button class="dropbtn">0
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="pages/0_booking.html">booking</a>
-          <a href="pages/0_goods.html">booking</a>
-          <a href="pages/0_tiamo.htmm">tiamo</a>
-        </div>
-      </div>
-      </li>
-   </ul>
-
-    <ul class="navbar__icons">
-      <li><i class="fa fa-youtube"></i></li>
-      <li><i class="fa fa-instagram"></i></li>
-    </ul>
-</nav>
+<script>
+    import { Button, Dropdown, DropdownItem, Chevron } from 'flowbite-svelte'
+  </script>
+  
+  <Button><Chevron>Dropdown button</Chevron></Button>
+  <Dropdown >
+    <DropdownItem><a href="/code">code</a></DropdownItem>
+    <DropdownItem>Settings</DropdownItem>
+    <DropdownItem>Earnings</DropdownItem>
+    <DropdownItem>Sign out</DropdownItem>
+  </Dropdown>
 <slot></slot>
