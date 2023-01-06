de60b9a42cfd80797afd26f063b99f6007578d97
choebyeong-gwan <ipschool22@gmail.com>
2023-01-06 18:08:45 +0900

    11commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="style.css">
     <script src="main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital@1&display=swap" rel="stylesheet">
     <script src="js/includeHTML.js"></script>
   </head>
@@ -64,81 +64,7 @@
     </ul>
 </nav>
 <style>
-    table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  font-size:4vw;
-}
-
-th {
-  border: 1px solid #dddddd;
-  border-radius: 50% 20% / 10% 40%;
-  text-align: center;
-  padding: 1px;
-  background-color: #041a4f;
-  cursor: pointer;
-  color: white;
-  position: sticky;
-  top: 0; 
-  box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
-}
-
-th:hover {
-    background-color:#041a4f;
-    color: white;    
-
-  }
   
-td{
-   text-align: center;
-   padding: 1px;
-    border: 2px solid #000000;
-  }
-
-tr:nth-child(even) {
-  background-color: #fcfadc
-  ;
-}
-  
-tr:hover {
-    background-color:#def3ff;
-    color: black;      
-}
-
-.special-image {
-    border-radius: 15px;
-}
-
-.collapsible {
-  background-color: #777;
-  color: white;
-  cursor: pointer;
-  padding: 18px;
-  width: 100%;
-  border: none;
-  text-align: left;
-  outline: none;
-  font-size: 15px;
-}
-
-.active, .collapsible:hover {
-  background-color: #555;
-}
-
-.content {
-  padding: 0 18px;
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s ease-out;
-  background-color: #f1f1f1;
-}
-
-body {
-  margin: 0;
-  font-family: 'Source Sans Pro', sans-serif;
-}
-
 .navbar {
   overflow: hidden;
   display: flex;
--- src/routes/code/+page.svelte
+++ src/routes/code/+page.svelte
@@ -1,1 +1,186 @@
-<h1>code_study</h1>
+  <head>
+    
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital@1&display=swap" rel="stylesheet">
+    <script src="js/includeHTML.js"></script>
+    <style>
+        table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  font-size:4vw;
+}
+
+th {
+  border: 1px solid #dddddd;
+  border-radius: 50% 20% / 10% 40%;
+  text-align: center;
+  padding: 1px;
+  background-color: #041a4f;
+  cursor: pointer;
+  color: white;
+  position: sticky;
+  top: 0; 
+  box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+}
+
+th:hover {
+    background-color:#041a4f;
+    color: white;    
+
+  }
+  
+td{
+   text-align: center;
+   padding: 1px;
+    border: 2px solid #000000;
+  }
+
+tr:nth-child(even) {
+  background-color: #fcfadc
+  ;
+}
+  
+tr:hover {
+    background-color:#def3ff;
+    color: black;      
+}
+
+.special-image {
+    border-radius: 15px;
+}
+
+.collapsible {
+  background-color: #777;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active, .collapsible:hover {
+  background-color: #555;
+}
+
+.content {
+  padding: 0 18px;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+  background-color: #f1f1f1;
+}
+
+body {
+  margin: 0;
+  font-family: 'Source Sans Pro', sans-serif;
+}
+    </style>
+  </head>
+  <body>
+
+    <h1>#Skill_#Code</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>C</th>
+          <th>C</th>  
+          <th>C</th>
+          <th>T</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <i class="fab fa-js"></i> 
+            <br>
+            JavaScript
+          </td>
+          <td>
+            <i class="fa-brands fa-square-js"></i>
+            <br>
+            JavaScript
+          </td>
+          <td></td> 
+          <td>
+            1.48
+          </td>         
+        </tr>
+        <tr>
+          <td>
+            <i class="fab fa-js"></i> 
+            <br>
+            JavaScript
+          </td>
+          <td>
+            <i class="fa-brands fa-react"></i>
+            <br>
+            React
+          <br>
+         </td>
+         <td></td>
+        <td>
+          5.41
+        </td>          
+      </tr>
+    <tr>
+     <td rowspan="2">
+      Python
+      <br>
+     </td>
+     <td>
+      Python
+      <br>
+     </td>
+     <td></td>
+    <td>
+      6.68
+    </td>          
+  </tr>
+<tr>
+ <td>
+  Flask
+  <br>
+ </td>
+ <td></td>
+<td>
+  0.27
+</td>          
+</tr>    
+<tr>
+  <td>
+    Artificial neural network
+    <br>
+   </td>
+  <td>
+   TebnsorFlow
+   <br>
+  </td>
+  <td>
+   Keras
+   <br>
+  </td>
+ <td>
+   0.26
+ </td>          
+</tr>
+  <tr>
+    <td>
+     HTML
+     <br>
+    </td>
+    <td></td>
+    <td></td>
+   <td>
+     1.56
+   </td>          
+ </tr>
+      </tbody>
+    </table>
+</body>
